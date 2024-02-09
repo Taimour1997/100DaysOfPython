3bb9c7787cfb0863b746e9c15a9d8f70cb4ec52a
Taimour1997 <shami.taimour@gmail.com>
2024-02-09 12:45:35 +0500

    Completed the project

---
+++ Desktop/100 Days Of Python Scripts/Day_1.py
@@ -0,0 +1,7 @@
+##### DAY 1 Project : BAND NAME GENERATOR ####
+print ("Welcome to the Project Band Name Generator\n") # The title of the project
+a=input("What's the name of the city you grew up in?\n") # Input the Name of the city
+# print(a)
+b=input("What's the name of your pet?\n") # Input the name of the pet
+# print(b)
+print("Your Band Name could be "+a+" "+b) # The Band Name at its output
